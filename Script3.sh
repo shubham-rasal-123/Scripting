bba79aff0bb991f0200a8baf20acd4c04629dcbc
Shubham Rasal <128680209+shubham-rasal-123@users.noreply.github.com>
2026-06-12 16:28:24 +0530

    Update Script3.sh

--- Script3.sh
+++ Script3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#this is to check if the shell scripts picks up env variables
-echo "This is error script"   # sample script
-echo "This is my system path $PATH"
+#this is to check if the shell scripts pick up env variables
+echo "This is an error script"   # sample script
+echo "This is my system path $PATH."
 
 #Set a JDK_HOME env variable
 export JDK_HOME=/bin/jdk
-echo "my new JDK home is=$JDK_HOME"
+echo "my new JDK home is=$JDK_HOME."
