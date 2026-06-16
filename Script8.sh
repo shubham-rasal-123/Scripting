3a45d865f5a1955ce4becac130cbab0dabdda8f4
Shubham Rasal <128680209+shubham-rasal-123@users.noreply.github.com>
2026-06-16 09:40:37 +0530

    Fix echo statements and condition message formatting

--- Script8.sh
+++ Script8.sh
@@ -5,11 +5,11 @@ b=20
 val1=`expr $a + $b`
 echo "a + b : $val1"
 val2=`expr $a - $b`
-echo "a - b : $val2"
+echo "a - b: $val2"
 val3=`expr $a \* $b`
 echo "a * b : $val3"
 val4=`expr $b / $a`
-echo "b / a : $val4"
+echo "b / a: $val4"
 val5=`expr $b % $a`
 echo "b % a : $val5"
 
@@ -25,6 +25,5 @@ elif [ $a -lt $b ]
 then
  echo "a is less than b"
 else
- echo "None of the condition met"
+ echo "None of the conditions met"
 fi
-
